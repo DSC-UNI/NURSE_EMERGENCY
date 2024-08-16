37eb96cbc9497dd1ec7cf10a0b713f9bab1cad82
Roberto Rodriguez Eduardo <eduardororo285@gmail.com>
2024-08-16 12:39:59 -0500

    Add Readme.md file

---
+++ Readme.md
@@ -0,0 +1,34 @@
+# File with heading
+
+This is a file with top-level heading
+
+## Supabase Functions
+
+Lista de Supabase Functions que se usan en la NurseEmergency App
+
+### Generate PDF Function
+
+Genera un PDF a partir de la información:
+    -Tabla de Cambio_Turno
+    -Almacenamiento de los recursos (firmas y logo)
+
+Los parámetros que recibe la función:
+    -Nombre completo del enfermero solicitante: string
+    -ID del enfermero solicitante: string
+    -Nombre completo del enfermero reemplazante: string
+    -ID del enfermero reemplazante: string
+    -Nombre del servicio: string
+    -Día que se realiza la solicitud: date
+    -Fecha y Hora del cambio del turno: datetime
+    -Razón de la solicitud: string
+    -Firma de la Coordinadora del servicio: image
+    -Firma de la Jefa de enfermería: image
+    -Logo de essalud: image
+
+### Send Notification Function
+
+Soon...
+
+### Put your function
+
+Write your function.
--- supabase/functions/generate_pdf/index.ts
+++ supabase/functions/generate_pdf/index.ts
@@ -34,18 +34,18 @@ Deno.serve(async (req) => {
       const signBuffer = Buffer.from(signArrayBuffer)
 
       console.log(data_);
-      const date_today = new Date(Date.now());
-      const string_today = date_today.toLocaleDateString("es-PE");
       const solicitante_fullname = "MARIA BELEN MALPARTIDA CRUZADO";
       const solicitante_id = "98765432";
-      const servicio_name = "Sala de Operaciones";
+      const reemplazante_fullname = "FERNANDA DENISSE ALMIRON ALAYA";
+      const reemplazante_id = "98765432";
+      const date_today = new Date(Date.now());
+      const string_today = date_today.toLocaleDateString("es-PE");
       
+      const servicio_name = "Sala de Operaciones";
       const horarioTurno = "07:00-13:00";
       const dateTurno = new Date(2002, 9, 10);
   
       const razonSolicitud = "EVENTOS FAMILIARES";
-      const reemplazante_fullname = "FERNANDA DENISSE ALMIRON ALAYA";
-      const reemplazante_id = "98765432";
   
       const doc = new PDFDocument({
           font: 'Times-Roman',
@@ -100,16 +100,3 @@ Deno.serve(async (req) => {
         });
     }
 })
-
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/generate_pdf' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
